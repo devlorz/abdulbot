d4d4c2e246a3995ad9233c2b088fe235266954cf
Nutti Saelor <leelorz6@gmail.com>
2018-03-21 10:53:51 +0700

    add logs

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ function reply(reply_token, msg) {
         },
         (err, res, body) => {
           console.log('status = ' + res.statusCode);
+          console.log('body = ' + body);
+          console.log('error = ', error);
         }
       );
     }
