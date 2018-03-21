466d51fa60fbfbe4d82cca3541cad3e9bcd3d076
Nutti Saelor <leelorz6@gmail.com>
2018-03-21 10:28:15 +0700

    add wiki api

--- app.js
+++ app.js
@@ -18,23 +18,30 @@ function reply(reply_token, msg) {
     Authorization:
       'Bearer {QNvmXR/WTbjEOqzSaK/GvT2M7GxnWesBl8QjXI+R+QTc9XfNEBM7W45RNzOKI8XdsNKHGR6VBOugXDetz3B6VDjzhAcY7DPS7h0Z4lWHWTP0I9aRVu+aCMotpLrcm2br/yv38ZTUTEk5Od3DbcD6bwdB04t89/1O/w1cDnyilFU=}'
   };
-  let body = JSON.stringify({
-    replyToken: reply_token,
-    messages: [
-      {
-        type: 'text',
-        text: msg
-      }
-    ]
-  });
-  request.post(
+  request.get(
     {
-      url: 'https://api.line.me/v2/bot/message/reply',
-      headers: headers,
-      body: body
+      url: `https://en.wikipedia.org/w/api.php?action=opensearch&search=${msg}&limit=1&namespace=0&format=jsonfm`
     },
-    (err, res, body) => {
-      console.log('status = ' + res.statusCode);
+    (error, response, body) => {
+      const replyBody = JSON.stringify({
+        replyToken: reply_token,
+        messages: [
+          {
+            type: 'text',
+            text: body
+          }
+        ]
+      });
+      request.post(
+        {
+          url: 'https://api.line.me/v2/bot/message/reply',
+          headers: headers,
+          body: replyBody
+        },
+        (err, res, body) => {
+          console.log('status = ' + res.statusCode);
+        }
+      );
     }
   );
 }
